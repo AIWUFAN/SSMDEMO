51c7018990caa24bd93ed4a80295607b01ecd101
AIWUFAN <git@github.com:AIWUFAN>
2018-01-21 18:11:03 +0800

    create user bean

---
+++ src/main/java/com/spring/learn/model/User.java
@@ -0,0 +1,39 @@
+package com.spring.learn.model;
+
+
+import java.io.Serializable;
+
+/**
+ * @author
+ * @create 2018-01-21 18:00
+ **/
+public class User implements Serializable {
+    private static final long serialVersionUID = -3053873592606646226L;
+    private String userId;
+    private String username;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
